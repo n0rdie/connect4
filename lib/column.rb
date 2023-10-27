5ad23e9425608ecf015cab052dc9e0806ea63b98
Quin Nordmark <quinnordmark@gmail.com>
2023-10-27 09:15:10 -0700

    Column .full? .fill_cells and .find_first_empty_cell methods [UNTESTED] (my VSCode won't update my code and I don't know why) git status

--- lib/column.rb
+++ lib/column.rb
@@ -5,5 +5,33 @@ class Column
     def initialize
         @cells = []
         @full = false
+        fill_cells
+    end
+
+    def fill_cells
+        6.times do
+            @cells.append(Cell.new)
+        end
+    end
+
+    def full?
+        if @cells[-1].empty? == false
+            @full = true
+        end
+        @full
+    end
+
+    def find_first_empty_cell
+        if full?
+            nil
+        else
+            first_empty_cell = nil
+            @cells.reverse_each do |cell|
+                if cell.empty?
+                    first_empty_cell = cell
+                end
+            end
+            first_empty_cell
+        end
     end
 end
