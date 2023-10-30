3cfef6e8f39f3a96ddce4a2147d5e66a469648d9
Martin Chavez <martinchavez1203@gmail.com>
2023-10-30 12:03:04 -0600

    start of gamestate class

--- lib/gamestate.rb
+++ lib/gamestate.rb
@@ -3,3 +3,6 @@ require './lib/cell'
 require './lib/column'
 require './lib/board'
 
+class GameState
+  
+end
--- spec/gamestate_spec.rb
+++ spec/gamestate_spec.rb
@@ -4,6 +4,14 @@ require './lib/column'
 require './lib/board'
 require './lib/gamestate'
 
-RSpec.describe Gamestate do
+RSpec.describe GameState do
+  before(:each) do
 
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect().to eq()
+    end
+  end
 end
