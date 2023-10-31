01146a5f1b43f1c4d633060f2722dfcd2ad8f89d
Quin Nordmark <quinnordmark@gmail.com>
2023-10-31 13:38:36 -0700

    Gamestate testing and methods

--- lib/gamestate.rb
+++ lib/gamestate.rb
@@ -2,4 +2,62 @@ require 'colorize'
 require './lib/cell'
 require './lib/column'
 require './lib/board'
+require './lib/gamestate'
 
+class Gamestate
+    attr_reader :ai, :board
+    def initialize
+        @ai = Gamestate.new
+        @board = Board.new
+    end
+
+    def computer_turn
+        @ai.place_random(@board)
+    end
+
+    def human_turn(input)
+        @board.place_cell("Human", input)
+    end
+
+    def check_winner(player)
+        num = 0
+        def count
+            num += 1
+            if num == 4
+                return true
+            end
+        end
+        
+        # vertical
+        board.columns.each do |column|
+            column.cells.each do |cell|
+                if cell.find_player_owner == player
+                    count
+                else
+                    num = 0
+                end
+            end
+            num = 0
+        end
+
+        # horizontal
+        for x in 0..board.columns.length-1
+            board.columns.each do |column|
+                if column.cells[x].find_player_owner == player
+                    count
+                else
+                    num = 0
+                end
+            end
+            num = 0
+        end
+
+        # up diaganal
+        # will do l8r
+
+        # down diaganal
+        # will do l8r
+
+        false
+    end
+end
--- spec/gamestate_spec.rb
+++ spec/gamestate_spec.rb
@@ -5,5 +5,23 @@ require './lib/board'
 require './lib/gamestate'
 
 RSpec.describe Gamestate do
+    it "can do human turn"
+        gamestate = Gamestate.new
+        expect(Gamestate.board.columns[0].cells[0].find_player_owner).to eq("empty")
+        gamestate.human_turn("A")
+        expect(Gamestate.board.columns[0].cells[0].find_player_owner).to eq("Human")
+    end
 
+    it "can do computer turn"
+        gamestate = Gamestate.new
+        gamestate.computer_turn
+        
+        passed = false
+        board.columns.each do |column|
+            if column.cells[0].find_player_owner == "Computer"
+                passed = true
+            end
+        end
+        expect(passed).to eq(true)
+    end
 end
