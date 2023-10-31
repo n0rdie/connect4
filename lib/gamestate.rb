52c2bbdef54ac189818f1d325657eb549483b68a
Martin Chavez <martinchavez1203@gmail.com>
2023-10-31 08:54:01 -0600

    start of display method

--- lib/gamestate.rb
+++ lib/gamestate.rb
@@ -3,6 +3,16 @@ require './lib/cell'
 require './lib/column'
 require './lib/board'
 
-class GameState
-  
-end
+class Gamestate
+  attr_reader :board
+
+  def initialize
+    @board = Board.new
+  end
+
+  def display
+    puts " A B C D E F G"
+    puts "+----------------"
+  end 
+
+end 
--- spec/gamestate_spec.rb
+++ spec/gamestate_spec.rb
@@ -4,14 +4,16 @@ require './lib/column'
 require './lib/board'
 require './lib/gamestate'
 
-RSpec.describe GameState do
+RSpec.describe Gamestate do
   before(:each) do
-
+    gamestate = Gamestate.new
   end
-
+  
   describe '#initialize' do
     it 'exists' do
-      expect().to eq()
+      gamestate = Gamestate.new
+      gamestate.display
+      
     end
   end
 end
