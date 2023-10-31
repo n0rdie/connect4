1a715df90b8cdf9e2ab49f47b222f63515a5e817
Martin Chavez <martinchavez1203@gmail.com>
2023-10-31 08:57:18 -0600

    #display method getting columns

--- lib/gamestate.rb
+++ lib/gamestate.rb
@@ -13,6 +13,13 @@ class Gamestate
   def display
     puts " A B C D E F G"
     puts "+----------------"
+
+    (0..5).reverse_each do |row_index|
+      row_string = "|"
+      @board.columns.cells[row_index]
+      row_string += "#{cell_display_value(cell)} "
+      require 'pry'; binding.pry
+    end
   end 
 
 end 
